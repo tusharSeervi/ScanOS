1b3d32ec6076f8caefdae165bf89e3df5d653f00
Tushar <tushar432seervi@gmail.com>
2026-07-06 13:09:49 +0530

    chore(frontend): configure production API endpoint

--- frontend/lib/axios.ts
+++ frontend/lib/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     process.env.NEXT_PUBLIC_API_BASE_URL ??
-    "http://127.0.0.1:8000/api",
+    "https://scanos-backend.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
